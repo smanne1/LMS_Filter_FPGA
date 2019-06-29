5e2f539282a6ab405dd17decc8855e180db15071
saisravanmanne <38082902+saisravanmanne@users.noreply.github.com>
2019-06-28 20:06:19 -0700

    Add files via upload

---
+++ lms_fpga_discrete.m
@@ -0,0 +1,63 @@
+clc
+close all
+clear all
+fps=100
+%N=input('interval = ');
+N=8
+t=[0:0.1:N-1];
+filter_size=length(t);
+filter_size=8;
+w0=0.5;  phi=0;
+desired_signal=[0 1 0 1 0 1 0 1]*fps;
+input_signal=[1 1 0 1 1 0 1 1]*fps;
+
+%desired_signal=10*sin(2*pi*[0:0.1:N-1]*w0+phi);
+%input_signal=10*si11n(2*pi*[0:0.1:N-1]*w0+phi).^2;
+
+
+filter_current = zeros(filter_size,1);
+input_vector = zeros(filter_size, 1);
+
+for k=1:1
+   
+for i=1:filter_size
+i
+
+input_vector(1)=input_signal(i);
+filter_output(i)=dot(filter_current, input_vector)/fps
+koutput(i)=filter_output(i);
+error= desired_signal(i)-filter_output(i)
+
+step_size=round(fps*fps/(0.001*fps+(dot(input_vector, input_vector))/fps))
+%step_size=2*0.0000001
+filter_current = filter_current +round(step_size*error*input_vector/(fps*fps))
+
+for j=filter_size:-1:2
+input_vector(j)=input_vector(j-1);
+end
+error_signal(i)=error;
+cost(i)=error*error;
+ss(i)=step_size;
+end
+input_vector = zeros(filter_size, 1)
+end
+%output=conv((filter_current)',input_signal)/(fps*fps)
+%koutput=output(1:10)
+%for i=1:filter_size
+%input_vector(1)=input_signal(i);
+%koutput(i)=dot(filter_current, input_vector)/(fps*fps);
+%for j=filter_size:-1:2
+%input_vector(j)=input_vector(j-1);
+%end
+%end
+
+t=0:7
+subplot(321),plot(t,desired_signal),ylabel('Desired Signal'),
+subplot(322),plot(t,input_signal),ylabel('Input Signal+Noise'),
+subplot(323),plot(t,error_signal),ylabel('Error')
+subplot(324),plot(t,koutput),ylabel('Adaptive Desired output')
+subplot(325),plot(t,ss),ylabel('Step-size')
+
+
+
+close all
